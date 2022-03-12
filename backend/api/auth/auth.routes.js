3d12b306fab313e1d4f460dc1181c83b3f62d650
Aviv <AvivRahamim95@gmail.com>
2022-03-12 13:54:52 +0200

    restructured endpoints and added an update to viewed_listings in the database of the user when he is logging in or requested from the database

--- backend/api/auth/auth.routes.js
+++ backend/api/auth/auth.routes.js
@@ -3,6 +3,8 @@ const passport = require('passport');
 const keys = require('../../keys');
 const {google} = require("googleapis");
 const User = require('./auth.service');
+const ItemServices = require('../items/item.service');
+const {request} = require("express");
 
 
 const redirectURL = 'http://localhost:3000'
@@ -124,7 +126,7 @@ router.get('/google', passport.authenticate('google', {
 router.get('/google/redirect', passport.authenticate('google'
     // successRedirect: redirectURL,
     // failureRedirect: redirectURL,
-), (req, res) => {
+), async (req, res) => {
     try {
         let categoryCounter = {
             furniture: 0,
@@ -140,156 +142,154 @@ router.get('/google/redirect', passport.authenticate('google'
         let friends_viewed_listings = {}
 
         // get user id
-        User.findById(req.user.id)
-            .then((result) => {
-                // save tokens for Google API access
-                const tokens = result.tokens;
-                const client_secret = keys.google.GOOGLE_CLIENT_SECRET;
-                const client_id = keys.google.GOOGLE_CLIENT_ID;
-                // const redirect_uris = redirectURL;
-                const oAuth2Client = new google.auth.OAuth2(
-                    client_id,
-                    client_secret,
-                    redirectURL
-                )
-                oAuth2Client.setCredentials(tokens)
+        let result = await User.findById(req.user.id)
+        const tokens = result.tokens;
+        const client_secret = keys.google.GOOGLE_CLIENT_SECRET;
+        const client_id = keys.google.GOOGLE_CLIENT_ID;
 
-                const people = google.people({version: 'v1', auth: oAuth2Client});
-                const gmail = google.gmail({version: "v1", auth: oAuth2Client});
-                // gets the list of mails from the user's Gmail
-                (() => {
-                    return new Promise((resolve, reject) =>
-                        gmail.users.messages.list({userId: req.user.googleId}, (err, result) => {
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id,
+            client_secret,
+            redirectURL
+        )
+        oAuth2Client.setCredentials(tokens);
+
+        const people = google.people({version: 'v1', auth: oAuth2Client});
+        const gmail = google.gmail({version: 'v1', auth: oAuth2Client});
+        // gets the list of mails from the user's Gmail
+        let gmail_list_result = new Promise((resolve, reject) => {
+            gmail.users.messages.list({userId: req.user.googleId}, async (err, result) => {
+                if (err) {
+                    reject(err);
+                }
+                // iterates on the last 30 messages or fewer if there are fewer messages
+                let messages_length = result.data.messages.length;
+                let mail_iterations = 30;
+                if (messages_length < mail_iterations) {
+                    mail_iterations = messages_length;
+                }
+                let promise_list = []
+                for (let i = 0; i < mail_iterations; i++) {
+                    // get a specific mail by its id
+                    promise_list.push(new Promise((resolve, reject) => {
+                        gmail.users.messages.get({
+                            userId: req.user.googleId,
+                            id: result.data.messages[i].id
+                        }, (err, res) => {
                             if (err) {
                                 reject(err);
                             }
-                            // iterates on the last 30 messages or fewer if there are fewer messages
-                            let messages_length = result.data.messages.length;
-                            let mail_iterations = 30
-                            if (messages_length < mail_iterations) {
-                                mail_iterations = messages_length;
-                            }
-                            for (let i = 0; i < mail_iterations; i++) {
-                                // get a specific mail by its id
-                                gmail.users.messages.get({
-                                    userId: req.user.googleId,
-                                    id: result.data.messages[i].id
-                                }, (err, request) => {
-                                    let title = request.data.payload.headers.find(x => x.name === 'Subject').value;
-                                    let lowerCaseTitle = title.toLowerCase();
-                                    for (let k in categoryDict) {
-                                        if (lowerCaseTitle.includes(k)) {
-                                            categoryCounter[categoryDict[k]] += 1;
-                                        }
-                                    }
-                                })
-                            }
-                            people.people.connections.list({
-                                resourceName: 'people/me',
-                                // pageSize: 2,
-                                personFields: 'names,emailAddresses',
-                            }, (err, res) => {
-                                if (res.data.connections){
-                                    const connections = res.data.connections;
-                                    // console.log('connections');
-                                    // console.log(connections);
-                                    // console.log('contacts emails: ' + connections[0]);
-                                    for (let i = 0; i < connections.length; i++) {
-                                        // console.log('person number ' + i + ' emails');
-                                        for (let j = 0; j < connections[i].emailAddresses.length; j++) {
-                                            // console.log('email number ' + j + ' on person');
-                                            // console.log(connections[i].emailAddresses[j].value);
-                                            friends_emails.push(connections[i].emailAddresses[j].value);
-                                        }
-                                    }
+                            let title = res.data.payload.headers.find(x => x.name === 'Subject').value;
+                            let lower_case_title = title.toLowerCase();
+                            for (let k in categoryDict) {
+                                if (lower_case_title.includes(k)) {
+                                    categoryCounter[categoryDict[k]] += 1;
                                 }
-                                resolve('ok');
-                            });
+                            }
+                            resolve();
                         })
-                    )
-                })()
-                    .then(() => {
-                        // update friends_emails to keep only emails of registered users
-                        for (let i of friends_emails) {
-                            User.findOne({email: i}).then((result) => {
-                                if (result) {
-                                    updated_friends_emails.push(i);
-                                    console.log('friends: ' + updated_friends_emails)
-                                    // add the viewed listings to the view_listings
-                                    if (result.viewed_listings) {
-                                        for (let k in result.viewed_listings) {
-                                            // if the listing is in the viewed_listings
-                                            if (friends_viewed_listings[k]) {
-                                                friends_viewed_listings[k] += result.viewed_listings[k];
-                                            } else {
-                                                friends_viewed_listings[k] = result.viewed_listings[k];
-                                            }
-                                        }
-                                    }
-                                }
-                            })
-                        }
-                        // deletes listings that do not exist anymore
-                        for(let k in friends_viewed_listings) {
-                            User.findById(k).then((result) => {
-                                if (!result) {
-                                    delete friends_viewed_listings[k];
-                                }
-                            })
-                        }
-                    })
-                    .then(() => {
-                        // finds the 3 most wanted categories for the user and update his profile in the database
-                        let categories_items = Object.keys(categoryCounter).map((key) => {
-                            return [key, categoryCounter[key]];
-                        });
-                        categories_items.sort((first, second) => {
-                            return second[1] - first[1];
-                        });
-                        let topCategories = [];
-                        for (let k = 0; k < 3; k++) {
-                            topCategories.push(categories_items[k][0]);
-                        }
-
-                        // finds the 5 (if possible) most viewed listings for updating user recommendation in database
-                        let listings_items = Object.keys(friends_viewed_listings).map((key) => {
-                            return [key, friends_viewed_listings[key]];
-                        });
-                        listings_items.sort((first, second) => {
-                            return second[1] - first[1];
-                        });
-                        let top_listings = [];
-                        let size = listings_items.length;
-                        if(size > 5) {
-                            size = 5
-                        }
-                        for (let k = 0; k < size; k++) {
-                            top_listings.push(listings_items[k][0]);
-                        }
-                        console.log('update')
-                        User.findByIdAndUpdate(
-                            {_id: req.user.id},
-                            {
-                                recommended_from_friends: top_listings,
-                                recommended_from_email: topCategories,
-                                friends_emails: updated_friends_emails
-                            })
-                            .then((result, err) => {
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    console.log('friends and recommendations updated');
-                                }
-                            })
-                    }).then(() => {
-                    res.redirect(redirectURL);
-                })
-                // res.redirect(redirectURL);
+                    }))
+                }
+                await Promise.all(promise_list);
+                resolve();
             })
-            .catch((err) => {
-                console.log(err);
-            });
-        // res.redirect(redirectURL);
+        })
+
+        let people_list_result = new Promise((resolve, reject) => {
+            people.people.connections.list({
+                resourceName: 'people/me',
+                personFields: 'names,emailAddresses',
+            }, (err, res) => {
+                if (res.data.connections) {
+                    const connections = res.data.connections;
+                    for (let i = 0; i < connections.length; i++) {
+                        for (let j = 0; j < connections[i].emailAddresses.length; j++) {
+                            friends_emails.push(connections[i].emailAddresses[j].value);
+                        }
+                    }
+                }
+                resolve();
+            })
+        })
+        await gmail_list_result;
+        await people_list_result;
+        // update friends_emails to keep only emails of registered users
+        for (let i of friends_emails) {
+            result = await User.findOne({email: i})
+            if (result) {
+                updated_friends_emails.push(i);
+                console.log('friends: ' + updated_friends_emails);
+                // add the viewed listings to the friends_viewed_listings
+                if (result.viewed_listings) {
+                    for (let k in result.viewed_listings) {
+                        // if the listing is in the friends_viewed_listings
+                        if (friends_viewed_listings[k]) {
+                            friends_viewed_listings[k] += result.viewed_listings[k];
+                        }
+                        else {
+                            friends_viewed_listings[k] = result.viewed_listings[k];
+                        }
+                    }
+                }
+            }
+        }
+        // delete from friends_viewed_listings listings that do not exist anymore
+        for (let k in friends_viewed_listings) {
+            result = await ItemServices.findById(k);
+            if (!result) {
+                delete friends_viewed_listings[k];
+            }
+        }
+        // finds the 3 most wanted categories for the user and updates his profile in the database
+        let categories_items = Object.keys(categoryCounter).map((key) => {
+            return [key, categoryCounter[key]];
+        });
+        categories_items.sort((first, second) => {
+            return second[1] - first[1];
+        });
+        let top_categories = [];
+        for (let k=0; k < 3; k++) {
+            top_categories.push(categories_items[k][0]);
+        }
+
+        // finds the 5 (if possible) most viewed listings for updating user recommendations in database
+        let listings_items = Object.keys(friends_viewed_listings).map((key) => {
+            return [key, friends_viewed_listings[key]];
+        });
+        listings_items.sort((first, second) => {
+            return second[1] - first[1];
+        });
+        let top_listings = [];
+        let size = listings_items.length;
+        if (size > 5) {
+            size = 5
+        }
+        for (let k = 0; k < size; k++) {
+            top_listings.push(listings_items[k][0]);
+        }
+        let user_viewed_listings = {}
+        result = await User.findById(req.user.id);
+        if(result.viewed_listings) {
+            user_viewed_listings = result.viewed_listings;
+            // delete from the users viewed_listings listings that do not exist anymore
+            for (let k in user_viewed_listings) {
+                result = await ItemServices.findById(k);
+                if (!result) {
+                    delete user_viewed_listings[k];
+                }
+            }
+        }
+        console.log('update');
+        result = await User.findByIdAndUpdate(
+            {_id: req.user.id},
+            {
+                viewed_listings: user_viewed_listings,
+                recommended_from_friends: top_listings,
+                recommended_from_email: top_categories,
+                friends_emails: updated_friends_emails
+            }
+        )
+        res.redirect(redirectURL);
     } catch (err) {
         res.status(500).json(err)
     }
@@ -319,7 +319,25 @@ router.put('/viewed_update', (req, res) => {
 })
 
 // returns the requested user by id from the database
-router.get('/user/:id', (req, res) => {
+router.get('/user/:id', async(req, res) => {
+    let user_viewed_listings = {}
+    let result = await User.findById(req.params.id);
+    if(result.viewed_listings) {
+        user_viewed_listings = result.viewed_listings;
+        // delete from the users viewed_listings listings that do not exist anymore
+        for (let k in user_viewed_listings) {
+            result = await ItemServices.findById(k);
+            if (!result) {
+                delete user_viewed_listings[k];
+            }
+        }
+    }
+    result = await User.findByIdAndUpdate(
+        {_id: req.params.id},
+        {
+            viewed_listings: user_viewed_listings,
+        }
+    )
     User.findById(req.params.id).then((result) => {
         res.send(result);
     })
